2c6fee516122610a05270a121ae76c68430b078d
Muiasar-Al-Ani <muiasar2012@gmail.com>
2021-10-07 16:33:18 -0400

    Added the getLastWorkout function that makes an API call to the api/workouts and returns the last from the workouts array

--- public/js/api.js
+++ public/js/api.js
@@ -1,4 +1,15 @@
-// API "Object" that contain all the front end api call functions
+// API "Object" that contains all the front end api call functions
 const API = {
-
+    // Makes an API call to the api/workouts and returns the last from the workouts array
+    async getLastWorkout() {
+        let res;
+        try {
+          res = await fetch("/api/workouts");
+        } catch (err) {
+          console.log(err)
+        }
+        const json = await res.json();
+    
+        return json[json.length - 1];
+      },
 }
