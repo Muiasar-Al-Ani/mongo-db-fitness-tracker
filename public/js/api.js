5be42045c5ed3f4c8437b9334eb29af8fb260900
Muiasar-Al-Ani <muiasar2012@gmail.com>
2021-10-07 16:36:48 -0400

    Added the addExercise functions that makes an API call to the api/workouts/withId route and update the identified workout with the new data

--- public/js/api.js
+++ public/js/api.js
@@ -1,4 +1,4 @@
-// API "Object" that contains all the front end api call functions
+// API Object that contains all the front end api call functions
 const API = {
     // Makes an API call to the api/workouts and returns the last from the workouts array
     async getLastWorkout() {
@@ -12,4 +12,20 @@ const API = {
     
         return json[json.length - 1];
       },
+
+      // Makes an API call to the api/workouts/withId route and update the identified workout with the new data 
+      async addExercise(data) {
+        const id = location.search.split("=")[1];
+    
+        const res = await fetch("/api/workouts/" + id, {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(data)
+        });
+    
+        const json = await res.json();
+    
+        return json;
+      },
+
 }
